d801ef219bce52d842ed2a83bd162f6ed69e2b37
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-05-17 00:45:23 +0900

    Initial Commit

---
+++ BookSearchApp/BookSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchAppApp.swift
+//  BookSearchApp
+//
+//  Created by AoiKobayashi on 2025/05/17.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
